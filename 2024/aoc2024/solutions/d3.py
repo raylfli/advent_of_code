4157c5bb9abef273af8a35b411cb2ecb2043a242
Raymond Li <34190736+raylfli@users.noreply.github.com>
2024-12-03 20:07:56 -0500

    Add 2024 d3 solutions

---
+++ 2024/aoc2024/solutions/d3.py
@@ -0,0 +1,43 @@
+"""
+Advent of Code - Day 3: Mull It Over
+"""
+
+import re
+
+
+def _extract_mul(s: str) -> list[(int, int)]:
+    """
+    Extract all valid mul(a,b) instances.
+    """
+    matches = re.findall(r'mul\((\d+?),(\d+?)\)', s)
+    return [(int(match[0]), int(match[1])) for match in matches]
+
+
+def _instruction_generator(s: str) -> str:
+    """
+    Yields characters from the instruction if they are outside of don't()/do() blocks.
+    """
+    enable = True
+    for i, c in enumerate(s):
+        if s[i:i + 7] == "don't()":
+            enable = False
+        elif s[i:i + 4] == "do()":
+            enable = True
+
+        if enable:
+            yield c
+
+
+def solve_part1(s: str) -> int:
+    """
+    Solve AoC D3 P1.
+    """
+    return sum(a * b for a, b in _extract_mul(s))
+
+
+def solve_part2(s: str) -> int:
+    """
+    Solve AoC D3 P2.
+    """
+    s = ''.join(_instruction_generator(s))
+    return solve_part1(s)
---
+++ 2024/tests/test_d3.py
@@ -0,0 +1,26 @@
+"""
+Advent of Code 2024 - Day 3 Tests
+"""
+
+from solutions.d3 import solve_part1, solve_part2
+
+
+class TestSuite:
+
+    pass
+
+
+class TestPart1(TestSuite):
+
+    INPUT_STR = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n"""
+
+    def test_1(self):
+        assert solve_part1(self.INPUT_STR) == 161
+
+
+class TestPart2(TestSuite):
+
+    INPUT_STR = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))\n"""
+
+    def test_1(self):
+        assert solve_part2(self.INPUT_STR) == 48
