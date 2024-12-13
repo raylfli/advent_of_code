9d83f4b09bd9ceb9ab1826241fe640033dea6eb2
Raymond Li <34190736+raylfli@users.noreply.github.com>
2024-12-13 17:16:07 -0500

    Add 2024 d13 solutions

---
+++ 2024/aoc2024/solutions/d13.py
@@ -0,0 +1,76 @@
+"""
+Advent of Code - Day 13: Claw Contraption
+"""
+
+import re
+
+type ButtonIncrement = tuple[int, int]
+type GridIndex = tuple[int, int]
+
+PATT_COORDINATE = re.compile(r'X[+=](\d+), Y[+=](\d+)')
+
+
+def _parse_setup(s: str, prize_incr: int = 0) -> tuple[
+    ButtonIncrement, ButtonIncrement, GridIndex]:
+    """
+    Parse single claw machine setup.
+    """
+    matches = re.findall(PATT_COORDINATE, s)
+
+    assert len(matches) == 3
+    return (
+        (int(matches[0][0]), int(matches[0][1])),
+        (int(matches[1][0]), int(matches[1][1])),
+        (prize_incr + int(matches[2][0]), prize_incr + int(matches[2][1]))
+    )
+
+
+def _parse_setups(s: str, prize_incr: int = 0) -> list[
+    tuple[ButtonIncrement, ButtonIncrement, GridIndex]]:
+    """
+    Parse claw machine setups.
+    """
+    machines = s.strip().split('\n\n')
+    return [_parse_setup(machine, prize_incr=prize_incr) for machine in machines]
+
+
+def _collect_prize(machine: tuple[ButtonIncrement, ButtonIncrement, GridIndex],
+                   max_presses: float = 100) -> int:
+    """
+    Find the minimum number of tokens to collect the prize. Return -1 if prize cannot be reached.
+    """
+    (ax, ay), (bx, by), (px, py) = machine
+
+    b = (ax * py - ay * px) / (ax * by - ay * bx)
+    a = (px - b * bx) / ax
+
+    if a.is_integer() and b.is_integer() and 0 <= a <= max_presses and 0 <= b <= max_presses:
+        return int(a * 3 + b)
+    else:
+        return -1
+
+
+def solve_part1(s: str) -> int:
+    """
+    Solve AoC D13 P1.
+    """
+    machines = _parse_setups(s)
+    total = 0
+    for machine in machines:
+        tokens = _collect_prize(machine)
+        if tokens >= 0:
+            total += tokens
+    return total
+
+
+def solve_part2(s: str) -> int:
+    """
+    Solve AoC D13 P2.
+    """
+    machines = _parse_setups(s, prize_incr=10000000000000)
+    total = 0
+    for machine in machines:
+        tokens = _collect_prize(machine, max_presses=float('inf'))
+        if tokens >= 0:
+            total += tokens
+    return total
---
+++ 2024/tests/test_d13.py
@@ -0,0 +1,36 @@
+"""
+Advent of Code 2024 - Day 13 Tests
+"""
+
+from solutions.d13 import solve_part1, solve_part2
+
+
+class TestSuite:
+    INPUT_STR = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"""
+
+
+class TestPart1(TestSuite):
+
+    def test_1(self):
+        assert solve_part1(self.INPUT_STR) == 480
+
+
+class TestPart2(TestSuite):
+
+    def test_1(self):
+        assert solve_part2(self.INPUT_STR) == 875318608908
